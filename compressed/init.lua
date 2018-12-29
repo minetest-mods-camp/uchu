32305289843d7e0dabc5a58c75a891138920b70d
Hume2 <teratux.mail@gmail.com>
2018-12-29 10:44:24 +0100

    Add compressed

---
+++ compressed/init.lua
@@ -0,0 +1,27 @@
+for id, s in pairs(underch.stone.defs) do
+	local cobble = "underch:" .. id .. "_cobble"
+	local compressed = "compressed:" .. id
+	local basename = minetest.registered_nodes["underch:"..id].description
+
+	minetest.register_node(compressed, {
+		description = "Compressed " .. basename .. " Cobble",
+		tiles = {"compressed_" .. id .. ".png"},
+		groups = {cracky = 3, stone = 1, smoothstone = 1},
+		sounds = default.node_sound_stone_defaults(),
+	})
+
+	minetest.register_craft({
+		output = compressed,
+		recipe = {
+			{cobble, cobble, cobble},
+			{cobble, cobble, cobble},
+			{cobble, cobble, cobble},
+		}
+	})
+
+	minetest.register_craft({
+		output = cobble .. " 9",
+		type = "shapeless",
+		recipe = {compressed}
+	})
+end
