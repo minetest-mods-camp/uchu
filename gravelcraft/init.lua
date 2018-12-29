6fa4e0c8aad98b65de21e18c52801e61cf53c593
Hume2 <teratux.mail@gmail.com>
2018-12-29 13:07:31 +0100

    Add gravelcraft

---
+++ gravelcraft/init.lua
@@ -0,0 +1,20 @@
+minetest.register_craft( {
+	output = "default:gravel 5",
+	recipe = {
+		{"group:cobble",       ""      ,"group:cobble"},
+		{     ""       , "group:cobble",     ""       },
+		{"group:cobble",       ""      ,"group:cobble"},
+	},
+})
+
+local have_compressed = minetest.get_modpath("compressed") ~= nil
+
+if technic ~= nil then
+	for id, s in pairs(underch.stone.defs) do
+		technic.register_grinder_recipe({input = {"underch:" .. id .. "_cobble"}, output = "default:gravel"})
+		technic.register_grinder_recipe({input = {"underch:" .. id}, output = "technic:stone_dust"})
+		if have_compressed then
+			technic.register_grinder_recipe({input = {"compressed:" .. id}, output = "default:gravel 9"})
+		end
+	end
+end
