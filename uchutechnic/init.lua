2971dd3d7620d7b0cc7a75fb28d27bf814d4eae7
Hume2 <teratux.mail@gmail.com>
2019-01-19 10:15:27 +0100

    uchutechnic: add recipe green mushroom x 10 -> green slimy block

--- uchutechnic/init.lua
+++ uchutechnic/init.lua
@@ -4,6 +4,8 @@ technic.register_grinder_recipe({input = {"underch:ruby"}, output = "underch:rub
 technic.register_compressor_recipe({input = {"underch:ruby_dust"}, output = "underch:ruby"})
 technic.register_compressor_recipe({input = {"underch:coal_dust"}, output = "default:coal_lump"})
 
+technic.register_extractor_recipe({input = {"underch:green_mushroom 10"}, output = "underch:green_slimy_block"})
+
 if minetest.get_modpath("ethereal") ~= nil then
 	technic.register_separating_recipe({ input = { "underch:fiery_dust 4" }, output = { "default:sand 3", "default:clay_lump 4", "ethereal:fire_dust" } })
 end
