0bebe6afe4b385bc6516ce6438e41a54bfeb074d
Hume2 <teratux.mail@gmail.com>
2022-05-02 12:48:12 +0200

    add uchu_stone

---
+++ uchu_stone/init.lua
@@ -0,0 +1,30 @@
+minetest.register_craft({
+	output = "default:stone",
+	type = "shapeless",
+	recipe = {"group:smoothstone"}
+})
+
+minetest.register_craft({
+	output = "default:cobble",
+	type = "shapeless",
+	recipe = {"group:cobble"}
+})
+
+minetest.register_craft({
+	output = "default:stonebrick",
+	type = "shapeless",
+	recipe = {"group:stonebrick"}
+})
+
+minetest.register_craft({
+	output = "default:stone_block",
+	type = "shapeless",
+	recipe = {"group:stoneblock"}
+})
+
+minetest.register_craft({
+	output = "default:mossycobble",
+	type = "shapeless",
+	recipe = {"group:mossycobble"}
+})
+
